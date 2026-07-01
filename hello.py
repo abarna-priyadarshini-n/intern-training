58c6eef472b5de203533f89a85f6fbfd6cd61496
ABARNA PRIYADARSHINI N <n.abarnapriyadarshini@gmail.com>
2026-07-01 11:20:02 +0530

    Add explanatory comments

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
+# Print a greeting
 print("Hello, welcome to Python!")
 
+# Store user information
 name = "Abarna Priyadarshini N"
 age = 26
 is_intern = True
 
+# Display the stored information
 print("Name:", name)
 print("Age:", age)
 print("Is Intern:", is_intern)
