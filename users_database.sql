be53fee0de3fa325ae6d252c52b8efdea6d7aa24
ABARNA PRIYADARSHINI N <n.abarnapriyadarshini@gmail.com>
2026-07-03 10:23:54 +0530

    Add SQL basics and PostgreSQL queries

--- users_database.sql
+++ users_database.sql
@@ -1,3 +1,39 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+INSERT INTO users (name, email)
+VALUES
+('Abarna', 'abarnagmail.com'),
+('Kutty', 'kutty@gmail.com'),
+('Sanjay', 'sanjay@yahoo.com'),
+('Janu', 'janu@yahoo.com'),
+('Sangeetha', 'sangeetha@gmail.com');
+
+SELECT * FROM users
+
+SELECT *
+FROM users
+WHERE name LIKE 'A%'
+ORDER BY id;
+
+SELECT *
+FROM users
+WHERE name LIKE 'S%'
+ORDER BY id;
+
+SELECT *
+FROM users
+WHERE id > 2
+ORDER BY id DESC;
+
+SELECT *
+FROM users
+WHERE email LIKE '%gmail%'
+ORDER BY name;
+
 SELECT *
 FROM users
 WHERE email LIKE '%yahoo%'
